26c4058dd0e6b11004d440d2a684b5066088f5b2
Jia Xu Li <lijiaxu350@gmail.com>
2026-06-23 18:47:32 +0000

    Modified google_auth.py and added to requirements.txt

--- google_auth.py
+++ google_auth.py
@@ -1,5 +1,5 @@
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
 from dotenv import load_dotenv
 import os
@@ -13,14 +13,22 @@ token_file = os.getenv("GOOGLE_TOKEN")
 
 def get_credentials():
   creds = None
-  if os.path.exists(token_file):
+  if os.path.exists(token_file): #if token.json exist already
     creds = Credentials.from_authorized_user_file(token_file, scopes)
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
-      flow = InstalledAppFlow.from_client_secrets_file(credentials_file, scopes)
-      creds = flow.run_console()
+      flow = Flow.from_client_secrets_file(credentials_file, scopes = scopes, redirect_uri = "urn:ietf:wg:oauth:2.0:oob")
+      auth_url, _ = flow.authorization_url(prompt = "consent")
+      print("Paste this in a new browser tab: ", auth_url)
+      code = input("Paste the code you received here: ")
+      flow.fetch_token(code = code)
+      creds = flow.credentials
     with open(token_file, "w") as token:
       token.write(creds.to_json())
   return creds
+
+# if __name__ == "__main__":            #test to see if connection works
+#   creds = get_credentials()
+#   print("Success: ", creds.valid)
